6956b34ad0632ed6744c396a55f8d6819eb340fa
QuenMBar <Qbeast360@gmail.com>
2021-02-22 14:21:24 -0800

    commit tree f64596822523674d34f48ce15a67627eb5687d8c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(dist) {
+    return Math.abs(42 - dist);
+}
+function distanceFromHqInFeet(dist) {
+    return Math.abs(42 - dist) * 264;
+}
+function distanceTravelledInFeet(dist1, dist2) {
+    return Math.abs(dist1 - dist2) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    let dist = distanceTravelledInFeet(start, destination);
+    if (dist < 400) {
+        return 0;
+    } else if (dist < 2000) {
+        return 0.02 * (dist - 400);
+    } else if (dist < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+
+}
